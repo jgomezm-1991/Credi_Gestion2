134addbe1a8eb236076ccb1a4d124e17b0fd3b49
jaime.gomez <jgomezm1991@gmail.com>
2022-02-19 11:28:54 -0600

    validar que guarde en la tabla cliente

--- Credi_Gestion/Models/Cliente.cs
+++ Credi_Gestion/Models/Cliente.cs
@@ -7,10 +7,18 @@ namespace Credi_Gestion.Models
     public class Cliente
     {
         [Key]
-        public int Id { get; set; }
+        public int IdCliente { get; set; }
 
         [Required(ErrorMessage = "El nombre del cliente es requerido")]
+
+
+        [StringLength(100)]
+
         public string NombreCliente { get; set; }
+
+
+        [StringLength(100)]
+        public string Apellidos {get ; set;}
 
         [Required(ErrorMessage = "El número de cedula es requerido")]
         [StringLength(100)]
@@ -22,27 +30,22 @@ namespace Credi_Gestion.Models
         public string Direccion { get; set; }
 
         [StringLength(100)]
-        public string Sexo { get;set;}
+        public string Genero { get;set;}
 
         [Required(ErrorMessage = "Debe ingresar el número de teléfono")]
         
         public string Telefono { get; set; }
 
-        [Required(ErrorMessage = "Debe ingresar el monto del préstamo")]
+        [StringLength(100)]
+        public string Estado { get; set; }
+
+
+
+
        
-        public decimal Prestamo { get; set; }
-
-        [Column(TypeName = "decimal(12, 2)")]
-        [Required(ErrorMessage = "Debe ingresar el saldo")]
-        public decimal Saldo { get; set; }
-
-        [Required(ErrorMessage = "Debe seleccionar la fecha")]
-        // guardar en la base de datos con fecha y hora
-        [DataType(DataType.Date)]
-        [Display(Name = "Fecha del registro")]
         public DateTime FechaReg { get; set; }
 
-        public string UsuarioRe { get; set; }
+        public string IdUsuario { get; set; }
 
 
     }
