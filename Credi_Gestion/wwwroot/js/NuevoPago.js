5ee4e482a118a20f602e5ea5de0215ac9f710b42
jaime.gomez <jgomezm1991@gmail.com>
2022-03-21 06:05:03 -0600

    Guardamos pago

---
+++ Credi_Gestion/wwwroot/js/NuevoPago.js
@@ -0,0 +1,71 @@
+﻿$("btnGuardar").click(function () {
+    var IDCLIENTE = $(".Idcliente").val();
+    var IDPRESTAMO = $(".IdPrestamo").val();
+    var Montopagado = $(".Montopagado").val();
+
+
+    if (Montopagado == "" || IDPRESTAMO == "" || IDCLIENTE == "") {
+
+
+        notif({
+
+            msg: "Todos los campos son requeridos",
+
+            type: "error"
+
+        });
+
+        return;
+    }
+    else {
+        var xhr = $.ajax({
+            url: "GuardarPago",
+            type: "POST",
+            data: {
+                "IdPrestamo": IDPRESTAMO,
+                "IdCliente": IDCLIENTE,
+                "Montopagado": Montopagado
+            }
+
+
+
+        });
+        xhr.done(function (data) {
+
+
+
+            notif({
+
+                msg: "Datos guardados Exitosamente",
+
+                type: "success"
+
+            });
+            //Swal.fire({
+            //    position: 'top-end',
+            //    icon: 'succes',
+            //    title: 'Datos Guardados Correctamente',
+            //    showConfirmButton: false,
+            //    timer: 1500
+            //})
+            //alert("Datos guardados correctamente");
+
+            $("Montopagado").val("");
+
+            setTimeout(function () {
+                location.href = "../Cliente/Pagos"
+            }, 600)
+
+        });
+        xhr.fail(function () {
+            Swal.fire({
+                title: 'Sweet!',
+                text: 'Modal with a custom image.',
+                imageUrl: 'https://unsplash.it/400/200',
+                imageWidth: 400,
+                imageHeight: 200,
+                imageAlt: 'Custom image',
+            })
+        });
+    }
+})
